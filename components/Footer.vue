278c7ba315bff8d08dbeace7932fb30898a9b822
xiaohi <shawinchina@icloud.com>
2018-01-21 22:40:43 +0800

    style

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,16 @@
 <template>
   <footer>
-    Copyleft©2018 版权所无
+  	<p>基于Laravel / Lumen、Express / Nuxt.js 驱动</p>
+    <p>Copyleft©2018 版权所无</p>
   </footer>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 	footer {
-		text-align: right;
+		font-size: 1.2rem;
+		p {
+			margin: 0;
+		}
 	}
+
 </style>
